e33196e3b10b4967ff6da54f0b64ac958ff5e5df
Liv Rickey <liv@snekbian.home.local>
2025-07-03 16:17:26 -0500

    font fixed

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "Cascadia Code NF:size=10";
-static char dmenufont[]       = "Cascadia Code NF:size=10";
+static char font[]            = "JetBrains Mono:size=10";
+static char dmenufont[]       = "JetBrains Mono:size=10";
 static const char *fonts[]          = { font };
 static char normbgcolor[]           = "#1f1d2e";
 static char normbordercolor[]       = "#2a273f";
